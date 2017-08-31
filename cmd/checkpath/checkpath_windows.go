5f49ceb5ae9094a352811a512259778be0fff8d7
Tristan Smith <triss@dreamlibrarian.com>
2017-08-31 13:13:48 -1000

    requested changes

--- cmd/checkpath/checkpath_windows.go
+++ cmd/checkpath/checkpath_windows.go
@@ -21,7 +21,5 @@ import (
 )
 
 func pathsOnSameDevice(p1, p2 string) bool {
-	id1 := filepath.VolumeName(p1)
-	id2 := filepath.VolumeName(p2)
-	return id1 == id2
+	return filepath.VolumeName(p1) == filepath.VolumeName(p2)
 }
