8411da26253734006862c448723697a0e7b42173
Nick Miyake <nmiyake@palantir.com>
2018-03-09 19:07:02 -0800

    Remove outdated comment

--- framework/builtintasks/verify.go
+++ framework/builtintasks/verify.go
@@ -43,7 +43,6 @@ func VerifyTask(tasks []godellauncher.Task) godellauncher.Task {
 
 	applyVar := cmd.Flags().Bool(apply, true, "Apply changes when possible")
 	for _, task := range verifyTasks {
-		// add "skip" flag for task. TODO: instead of adding a flag per task, create a single "skip" flag that takes parameters.
 		skipVerifyTasks[task.Name] = cmd.Flags().Bool("skip-"+task.Name, false, fmt.Sprintf("Skip '%s' task", task.Name))
 
 		flags, ok := verifyTaskFlags[task.Name]
