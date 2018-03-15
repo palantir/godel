89306949e97e569af87df26b6f4d44da01142e1e
Nick Miyake <nmiyake@palantir.com>
2018-03-15 14:48:12 -0700

    Update plugins and assets to latest versions

--- integration_test/integration_verify_test.go
+++ integration_test/integration_verify_test.go
@@ -22,6 +22,7 @@ import (
 	"path"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/nmiyake/pkg/gofiles"
 	"github.com/stretchr/testify/assert"
@@ -31,19 +32,9 @@ import (
 const (
 	licenseYML = `
 header: |
-  // Copyright 2016 Palantir Technologies, Inc.
-  //
-  // Licensed under the Apache License, Version 2.0 (the "License");
-  // you may not use this file except in compliance with the License.
-  // You may obtain a copy of the License at
-  //
-  // http://www.apache.org/licenses/LICENSE-2.0
-  //
-  // Unless required by applicable law or agreed to in writing, software
-  // distributed under the License is distributed on an "AS IS" BASIS,
-  // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  // See the License for the specific language governing permissions and
-  // limitations under the License.
+  // Copyright (c) {{YEAR}} Palantir Technologies Inc. All rights reserved.
+  // Use of this source code is governed by the Apache License, Version 2.0
+  // that can be found in the LICENSE file.
 `
 )
 
@@ -128,26 +119,21 @@ func TestVerifyApply(t *testing.T) {
 	const (
 		formattedTestSrc = `package main_test
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFoo(t *testing.T) {
 	t = t
 	t.Fail()
 }
 `
-		licensedTestSrc = `// Copyright 2016 Palantir Technologies, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+	)
+
+	var (
+		licensedTestSrc = fmt.Sprintf(`// Copyright (c) %d Palantir Technologies Inc. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
 
 package main_test
 	import "testing"
@@ -155,30 +141,22 @@ package main_test
 	func TestFoo(t *testing.T) {
 		t=t
 		t.Fail()
-	}`
-		licensedAndFormattedTestSrc = `// Copyright 2016 Palantir Technologies, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+	}`, time.Now().Year())
+		licensedAndFormattedTestSrc = fmt.Sprintf(`// Copyright (c) %d Palantir Technologies Inc. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
 
 package main_test
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFoo(t *testing.T) {
 	t = t
 	t.Fail()
 }
-`
+`, time.Now().Year())
 	)
 	err := ioutil.WriteFile(path.Join(testProjectDir, "godel", "config", "license.yml"), []byte(licenseYML), 0644)
 	require.NoError(t, err)
