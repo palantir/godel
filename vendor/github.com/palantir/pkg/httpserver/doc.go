29049b79d8bf8de0eb67cec8e2ab1dc87fbcdbf8
Nick Miyake <nmiyake@users.noreply.github.com>
2018-05-19 21:16:32 -0700

    Update dep configuration to prune unused packages (#392)

--- vendor/github.com/palantir/pkg/httpserver/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2016 Palantir Technologies. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package httpserver provides functions that are useful for HTTP servers. These functions are meant to be used
-// primarily in tests. Provides functions for determining an available port and checking to see when a server is "ready"
-// based on the response provided by an endpoint.
-package httpserver
--- vendor/github.com/palantir/pkg/tlsconfig/testdata/generate.go
+++
@@ -1,3 +0,0 @@
-//go:generate go run generator.go
-
-package testdata
