d246716ba2043d3b72cb8c5e094a4029017f8a3f
Brad Moylan <bmoylan@palantir.com>
2019-01-08 15:27:16 -0800

    Bump palantir/pkg dependency

--- vendor/github.com/palantir/pkg/matcher/config.go
+++ vendor/github.com/palantir/pkg/matcher/config.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Palantir Technologies. All rights reserved.
+// Copyright (c) 2016 Palantir Technologies. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
--- vendor/github.com/palantir/pkg/signals/signals.go
+++ vendor/github.com/palantir/pkg/signals/signals.go
@@ -1,10 +1,11 @@
-// Copyright 2016 Palantir Technologies. All rights reserved.
+// Copyright (c) 2016 Palantir Technologies. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package signals
 
 import (
+	"bytes"
 	"context"
 	"io"
 	"os"
@@ -13,7 +14,23 @@ import (
 	"syscall"
 )
 
-// ContextWithShutdown returns a context that is cancelled when the SIGTERM or SIGINT signal is received.
+// ContextWithShutdown returns a context that is cancelled when the SIGTERM or SIGINT signal is received. This can be
+// useful to use for cleanup tasks that would typically be deferred and which you want to run even if the program is
+// terminated or cancelled. The following is a common usage pattern:
+//
+//   cleanupCtx, cancel := signals.ContextWithShutdown(context.Background())
+//   cleanupDone := make(chan struct{})
+//   defer func() {
+//   	cancel()
+//   	<-cleanupDone
+//   }()
+//   go func() {
+//   	select {
+//   	case <-cleanupCtx.Done():
+//   		// perform cleanup action
+//   	}
+//   	cleanupDone <- struct{}{}
+//   }()
 func ContextWithShutdown(ctx context.Context) (context.Context, context.CancelFunc) {
 	return CancelOnSignalsContext(ctx, syscall.SIGTERM, syscall.SIGINT)
 }
@@ -44,26 +61,39 @@ func RegisterStackTraceWriter(out io.Writer, errHandler func(error)) (unregister
 // error, that error is provided to the errHandler function if one is provided. Returns a function that unregisters the
 // listener when called.
 func RegisterStackTraceWriterOnSignals(out io.Writer, errHandler func(error), sig ...os.Signal) (unregister func()) {
-	cancel := make(chan bool, 1)
-	unregister = func() {
-		cancel <- true
+	ctx, cancel := context.WithCancel(context.Background())
+	handler := func(stackTraceOutput []byte) error {
+		_, err := out.Write(stackTraceOutput)
+		return err
 	}
+	RegisterStackTraceHandlerOnSignals(ctx, handler, errHandler, sig...)
+	return cancel
+}
 
+// RegisterStackTraceHandlerOnSignals starts a goroutine that listens for the specified signals and calls stackTraceHandler with a
+// pprof-formatted snapshot of all running goroutines when any of the provided signals are received. If stackTraceHandler returns
+// an error, that error is provided to the errHandler function if one is provided. No goroutine is created if stackTraceHandler is nil.
+// If no signals are provided, the handler will receive notifications for all signals (matching the os/signal.Notify API).
+// The handler will exit when ctx is cancelled.
+func RegisterStackTraceHandlerOnSignals(ctx context.Context, stackTraceHandler func(stackTraceOutput []byte) error, errHandler func(error), sig ...os.Signal) {
+	if stackTraceHandler == nil {
+		return
+	}
 	signals := NewSignalReceiver(sig...)
 	go func() {
 		for {
 			select {
 			case <-signals:
-				err := pprof.Lookup("goroutine").WriteTo(out, 2)
-				if err != nil && errHandler != nil {
+				var buf bytes.Buffer
+				_ = pprof.Lookup("goroutine").WriteTo(&buf, 2) // bytes.Buffer's Write never returns an error, so we swallow it
+				if err := stackTraceHandler(buf.Bytes()); err != nil && errHandler != nil {
 					errHandler(err)
 				}
-			case <-cancel:
+			case <-ctx.Done():
 				return
 			}
 		}
 	}()
-	return unregister
 }
 
 // NewSignalReceiver returns a buffered channel that is registered to receive the provided signals.
