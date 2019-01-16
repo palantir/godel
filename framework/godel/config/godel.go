f079866881cacd9099f5c0b1566e2523f2a5ee56
Nick Miyake <nmiyake@users.noreply.github.com>
2019-01-16 12:50:02 -0800

    Remove old license header (#434)

--- framework/godel/config/godel.go
+++ framework/godel/config/godel.go
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright (c) 2016 Palantir Technologies Inc. All rights reserved.
-// Use of this source code is governed by the Apache License, Version 2.0
-// that can be found in the LICENSE file.
-
 package config
 
 import (
