df26a4b3deb9eba75afc2c5cbdf952a016d2d283
Nick Miyake <nmiyake@users.noreply.github.com>
2019-03-25 13:53:34 -0700

    Add ReadGodelConfigExcludesFromFile function (#450)
    Allows library to read only exclude portion of configuration.
    
    Fixes #447

--- framework/godel/config/readconfig.go
+++ framework/godel/config/readconfig.go
@@ -19,6 +19,7 @@ import (
 	"os"
 	"path"
 
+	"github.com/palantir/pkg/matcher"
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 
@@ -39,19 +40,46 @@ func ReadGodelConfigFromProjectDir(projectDir string) (GodelConfig, error) {
 // ReadGodelConfigFromFile reads the gödel configuration from the provided file and returns the loaded configuration.
 // Returns an empty configuration if the file does not exist.
 func ReadGodelConfigFromFile(cfgFile string) (GodelConfig, error) {
+	if _, err := os.Stat(cfgFile); os.IsNotExist(err) {
+		return GodelConfig{}, nil
+	}
+
 	var godelCfg GodelConfig
-	if _, err := os.Stat(cfgFile); err == nil {
-		bytes, err := ioutil.ReadFile(cfgFile)
-		if err != nil {
-			return GodelConfig{}, errors.Wrapf(err, "failed to read file %s", cfgFile)
-		}
-		upgradedBytes, err := UpgradeConfig(bytes)
-		if err != nil {
-			return GodelConfig{}, errors.Wrapf(err, "failed to upgrade configuration")
-		}
-		if err := yaml.Unmarshal(upgradedBytes, &godelCfg); err != nil {
-			return GodelConfig{}, errors.Wrapf(err, "failed to unmarshal gödel config YAML")
-		}
+	bytes, err := ioutil.ReadFile(cfgFile)
+	if err != nil {
+		return GodelConfig{}, errors.Wrapf(err, "failed to read file %s", cfgFile)
+	}
+	upgradedBytes, err := UpgradeConfig(bytes)
+	if err != nil {
+		return GodelConfig{}, errors.Wrapf(err, "failed to upgrade configuration")
+	}
+	if err := yaml.Unmarshal(upgradedBytes, &godelCfg); err != nil {
+		return GodelConfig{}, errors.Wrapf(err, "failed to unmarshal gödel config YAML")
 	}
 	return godelCfg, nil
+}
+
+// ReadGodelConfigExcludesFromFile reads the excludes specified in the gödel godel configuration from the provided file
+// and returns the loaded configuration. Returns an empty configuration if the file does not exist. Callers that only
+// require the exclude configuration should prefer this function to using the ReadGodelConfigFrom* functions and
+// accessing the exclude configuration there, as this function is more robust to configuration changes (for example, the
+// ReadGodelConfigFrom* functions will return an error if the configuration has an unrecognized key, while this function
+// only considers the "exclude" portion of configuration).
+func ReadGodelConfigExcludesFromFile(cfgFilePath string) (matcher.NamesPathsCfg, error) {
+	if _, err := os.Stat(cfgFilePath); os.IsNotExist(err) {
+		return matcher.NamesPathsCfg{}, nil
+	}
+
+	type excludeConfig struct {
+		Exclude matcher.NamesPathsCfg `yaml:"exclude,omitempty"`
+	}
+	cfgBytes, err := ioutil.ReadFile(cfgFilePath)
+	if err != nil {
+		return matcher.NamesPathsCfg{}, errors.WithStack(err)
+	}
+	var exclude excludeConfig
+	if err := yaml.Unmarshal(cfgBytes, &exclude); err != nil {
+		return matcher.NamesPathsCfg{}, errors.WithStack(err)
+	}
+	return exclude.Exclude, nil
 }
