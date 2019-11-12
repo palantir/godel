e7f50032d6e249e455f5e925a4d302811ff39264
Nick Miyake <nmiyake@palantir.com>
2019-11-12 10:47:10 -0800

    Fix tests

--- integration_test/integration_test.go
+++ integration_test/integration_test.go
@@ -254,7 +254,7 @@ func TestDebugFlagPrintsStackTrace(t *testing.T) {
 	cmd.Dir = testProjectDir
 	output, err = cmd.CombinedOutput()
 	require.Error(t, err)
-	assert.Regexp(t, `(?s)^Error: foo does not exist.+`+regexp.QuoteMeta(`github.com/palantir/godel/godelgetter.(*localFilePkg).Reader`)+`.+failed to install from foo into .+`, string(output))
+	assert.Regexp(t, `(?s)^Error: foo does not exist.+`+regexp.QuoteMeta(`github.com/palantir/godel/v2/godelgetter.(*localFilePkg).Reader`)+`.+failed to install from foo into .+`, string(output))
 }
 
 func execCommand(t *testing.T, dir, cmdName string, args ...string) string {
